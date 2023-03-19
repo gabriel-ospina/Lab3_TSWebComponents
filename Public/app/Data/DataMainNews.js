15204eef76e6fdf8209b78da8ae1b0bfe70a045b
gabriel-ospina <foxgdo@hotmail.com>
2023-03-19 13:28:59 -0500

    Docs: added main News Data

---
+++ Public/app/Data/DataMainNews.js
@@ -0,0 +1,15 @@
+const dataMainNews = [
+    {
+        image: "https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.25/c_scale,w_600/ncom/en_US/articles/2023/mobilenews-give-the-super-mario-run-game-a-go-with-daily-unlocks/SuperMarioRun_20230310",
+        date: "03/17/23",
+        tittle: "Give the Super Mario Run game a go with daily unlocks",
+        text: "Here we go! To celebrate the release of The Super Mario Bros. Movie, the Super Mario™ Run mobile game is sprinting forward with a fresh update and limited-time perks! The event period runs from now until May 30 at 11:59 p.m. PT. Free stage unlocks d…",
+    },
+    {
+        image: "https://assets.nintendo.com/image/upload/ar_16:9,b_auto:border,c_lpad/b_white/f_auto/q_auto/dpr_1.25/c_scale,w_600/ncom/en_US/articles/2023/bayonetta-origins-cereza-and-the-lost-demon-available-now/2250x1266_Ncom_Cereza",
+        date: "03/17/23",
+        tittle: "Bayonetta Origins: Cereza and the Lost Demon available now",
+        text: "Once upon a time… Long before she became the battle-ready Bayonetta, a young Cereza was cast out by both of her parents’ clans. Her only companion? A stuffed toy inhabited by a demon. Join Cereza and Cheshire, her very first demon, on a quest to sav…",
+    },
+];
+export default dataMainNews;
